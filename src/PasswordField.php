fa63794d821185c72129df67391e528c5bad26b7
Michael Hall <hall@michaelhall.se>
2017-05-15 21:23:58 +0200

    Add PasswordField class

---
+++ src/PasswordField.php
@@ -0,0 +1,249 @@
+<?php
+/**
+ * This file is a part of the bluemvc-forms package.
+ *
+ * Read more at https://bluemvc.com/
+ */
+
+namespace BlueMvc\Forms;
+
+use BlueMvc\Forms\Interfaces\FormElementInterface;
+
+/**
+ * Class representing a password field.
+ *
+ * @since 1.0.0
+ */
+class PasswordField implements FormElementInterface
+{
+    /**
+     * Constructs the password field.
+     *
+     * @since 1.0.0
+     *
+     * @param string $name The name.
+     *
+     * @throws \InvalidArgumentException If the $value parameters is not a string.
+     */
+    public function __construct($name)
+    {
+        if (!is_string($name)) {
+            throw new \InvalidArgumentException('$name parameter is not a string.');
+        }
+
+        $this->myName = $name;
+        $this->myValue = '';
+        $this->myError = null;
+        $this->myIsRequired = true;
+    }
+
+    /**
+     * Returns the element error or null if element has no error.
+     *
+     * @since 1.0.0
+     *
+     * @return string|null The element error or null if element has no error.
+     */
+    public function getError()
+    {
+        return $this->myError;
+    }
+
+    /**
+     * Returns the element html.
+     *
+     * @since 1.0.0
+     *
+     * @param array $attributes The attributes.
+     *
+     * @return string The element html.
+     */
+    public function getHtml(array $attributes = [])
+    {
+        return $this->buildTag('input',
+            array_merge(
+                [
+                    'type'     => 'password',
+                    'name'     => $this->myName,
+                    'required' => $this->myIsRequired,
+                ],
+                $attributes
+            )
+        );
+    }
+
+    /**
+     * Returns the element name.
+     *
+     * @since 1.0.0
+     *
+     * @return string The element name.
+     */
+    public function getName()
+    {
+        return $this->myName;
+    }
+
+    /**
+     * Returns the value of the text field.
+     *
+     * @since 1.0.0
+     *
+     * @return string The value of the text field.
+     */
+    public function getValue()
+    {
+        return $this->myValue;
+    }
+
+    /**
+     * Returns true if element has an error, false otherwise.
+     *
+     * @since 1.0.0
+     *
+     * @return bool True if element has an error, false otherwise.
+     */
+    public function hasError()
+    {
+        return $this->myError !== null;
+    }
+
+    /**
+     * Returns true if element value is empty, false otherwise.
+     *
+     * @since 1.0.0
+     *
+     * @return bool True if element value is empty, false otherwise.
+     */
+    public function isEmpty()
+    {
+        return $this->myValue === '';
+    }
+
+    /**
+     * Returns true if element value is required, false otherwise.
+     *
+     * @since 1.0.0
+     *
+     * @return bool True if element value is required, false otherwise.
+     */
+    public function isRequired()
+    {
+        return $this->myIsRequired;
+    }
+
+    /**
+     * Sets the element error.
+     *
+     * @since 1.0.0
+     *
+     * @param string $error The element error.
+     *
+     * @throws \InvalidArgumentException If the $error parameter is not a string.
+     */
+    public function setError($error)
+    {
+        if (!is_string($error)) {
+            throw new \InvalidArgumentException('$error parameter is not a string.');
+        }
+
+        $this->myError = $error;
+    }
+
+    /**
+     * Sets the value from form.
+     *
+     * @since 1.0.0
+     *
+     * @param string $value The value from form.
+     *
+     * @throws \InvalidArgumentException If the $value parameter is not a string.
+     */
+    public function setFormValue($value)
+    {
+        if (!is_string($value)) {
+            throw new \InvalidArgumentException('$value parameter is not a string.');
+        }
+
+        $this->myValue = $value;
+    }
+
+    /**
+     * Sets whether element value is required.
+     *
+     * @since 1.0.0
+     *
+     * @param bool $isRequired True if element value is required, false otherwise.
+     *
+     * @throws \InvalidArgumentException If the $isRequired parameter is not a boolean.
+     */
+    public function setRequired($isRequired)
+    {
+        if (!is_bool($isRequired)) {
+            throw new \InvalidArgumentException('$isRequired parameter is not a boolean.');
+        }
+
+        $this->myIsRequired = $isRequired;
+    }
+
+    /**
+     * Returns the element html.
+     *
+     * @since 1.0.0
+     *
+     * @return string The element html.
+     */
+    public function __toString()
+    {
+        return $this->getHtml();
+    }
+
+    /**
+     * Builds a tag from a name and attributes array.
+     *
+     * @since 1.0.0
+     *
+     * @param string $name       The name.
+     * @param array  $attributes The attributes.
+     *
+     * @return string The tag.
+     */
+    protected function buildTag($name, $attributes = [])
+    {
+        $result = '<' . htmlspecialchars($name);
+        foreach ($attributes as $attributeName => $attributeValue) {
+            if ($attributeValue === null || $attributeValue === false) {
+                continue;
+            }
+
+            $result .= ' ' . htmlspecialchars($attributeName);
+            if ($attributeValue === true) {
+                continue;
+            }
+
+            $result .= '="' . htmlspecialchars($attributeValue) . '"';
+        }
+
+        return $result . '>';
+    }
+
+    /**
+     * @var string My name.
+     */
+    private $myName;
+
+    /**
+     * @var string My value.
+     */
+    private $myValue;
+
+    /**
+     * @var string|null My error or null if no error.
+     */
+    private $myError;
+
+    /**
+     * @var bool If true element value is required, false otherwise.
+     */
+    private $myIsRequired;
+}
---
+++ tests/PasswordFieldTest.php
@@ -0,0 +1,212 @@
+<?php
+
+namespace BlueMvc\Forms\Tests;
+
+use BlueMvc\Forms\PasswordField;
+
+/**
+ * Test PasswordField class.
+ */
+class PasswordFieldTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test basic constructor.
+     */
+    public function testBasicConstructor()
+    {
+        $passwordField = new PasswordField('foo');
+
+        self::assertSame('<input type="password" name="foo" required>', $passwordField->getHtml());
+        self::assertSame('<input type="password" name="foo" required>', $passwordField->__toString());
+    }
+
+    /**
+     * Test constructor with invalid name parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $name parameter is not a string.
+     */
+    public function testConstructorWithInvalidNameParameterType()
+    {
+        new PasswordField(0);
+    }
+
+    /**
+     * Test getName method.
+     */
+    public function testGetName()
+    {
+        $passwordField = new PasswordField('foo');
+
+        self::assertSame('foo', $passwordField->getName());
+    }
+
+    /**
+     * Test getValue method.
+     */
+    public function testGetValue()
+    {
+        $passwordField = new PasswordField('foo');
+
+        self::assertSame('', $passwordField->getValue());
+    }
+
+    /**
+     * Test setFormValue method.
+     */
+    public function testSetFormValue()
+    {
+        $passwordField = new PasswordField('foo');
+        $passwordField->setFormValue('bar');
+
+        self::assertSame('bar', $passwordField->getValue());
+        self::assertSame('<input type="password" name="foo" required>', $passwordField->getHtml());
+        self::assertSame('<input type="password" name="foo" required>', $passwordField->__toString());
+    }
+
+    /**
+     * Test that setFormValue method does not trim the input.
+     */
+    public function testSetFormValueDoesNotTrimInput()
+    {
+        $passwordField = new PasswordField('foo');
+        $passwordField->setFormValue('bar');
+
+        self::assertSame('bar', $passwordField->getValue());
+        self::assertSame('<input type="password" name="foo" required>', $passwordField->getHtml());
+        self::assertSame('<input type="password" name="foo" required>', $passwordField->__toString());
+    }
+
+    /**
+     * Test setFormValue method with invalid value parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $value parameter is not a string.
+     */
+    public function testSetFormValueWithInvalidValueParameterType()
+    {
+        $passwordField = new PasswordField('foo');
+        $passwordField->setFormValue(true);
+    }
+
+    /**
+     * Test that output is html-encoded.
+     */
+    public function testOutputIsHtmlEncoded()
+    {
+        $passwordField = new PasswordField('<p>');
+
+        self::assertSame('<input type="password" name="&lt;p&gt;" required>', $passwordField->getHtml());
+        self::assertSame('<input type="password" name="&lt;p&gt;" required>', $passwordField->__toString());
+    }
+
+    /**
+     * Test isEmpty method for empty value.
+     */
+    public function testIsEmptyForEmptyValue()
+    {
+        $passwordField = new PasswordField('foo');
+
+        self::assertTrue($passwordField->isEmpty());
+    }
+
+    /**
+     * Test isEmpty method for non-empty value.
+     */
+    public function testIsEmptyForNonEmptyValue()
+    {
+        $passwordField = new PasswordField('foo');
+        $passwordField->setFormValue('bar');
+
+        self::assertFalse($passwordField->isEmpty());
+    }
+
+    /**
+     * Test hasError method.
+     */
+    public function testHasError()
+    {
+        $passwordField = new PasswordField('foo');
+
+        self::assertFalse($passwordField->hasError());
+    }
+
+    /**
+     * Test getError method.
+     */
+    public function testGetError()
+    {
+        $passwordField = new PasswordField('foo');
+
+        self::assertNull($passwordField->getError());
+    }
+
+    /**
+     * Test setError method.
+     */
+    public function testSetError()
+    {
+        $passwordField = new PasswordField('foo');
+        $passwordField->setError('My Error');
+
+        self::assertTrue($passwordField->hasError());
+        self::assertSame('My Error', $passwordField->getError());
+    }
+
+    /**
+     * Test setError method with invalid parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $error parameter is not a string.
+     */
+    public function testSetErrorWithInvalidParameterType()
+    {
+        $passwordField = new PasswordField('foo');
+        $passwordField->setError(1.2);
+    }
+
+    /**
+     * Test getHtml method with attributes.
+     */
+    public function testGetHtmlWithAttributes()
+    {
+        $passwordField = new PasswordField('foo');
+
+        self::assertSame('<input type="password" name="foo" required id="baz" readonly>', $passwordField->getHtml(['id' => 'baz', 'readonly' => true]));
+    }
+
+    /**
+     * Test isRequired method.
+     */
+    public function testIsRequired()
+    {
+        $passwordField = new PasswordField('foo');
+
+        self::assertTrue($passwordField->isRequired());
+    }
+
+    /**
+     * Test setRequired method.
+     */
+    public function testSetRequired()
+    {
+        $passwordField = new PasswordField('foo');
+        $passwordField->setRequired(false);
+
+        self::assertFalse($passwordField->isRequired());
+        self::assertSame('<input type="password" name="foo">', $passwordField->getHtml());
+        self::assertSame('<input type="password" name="foo">', $passwordField->__toString());
+    }
+
+    /**
+     * Test setRequired method with invalid parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $isRequired parameter is not a boolean.
+     */
+    public function testSetRequiredWithInvalidParameterType()
+    {
+        $passwordField = new PasswordField('foo');
+        $passwordField->setRequired(0);
+    }
+}
