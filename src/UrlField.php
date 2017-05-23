538a59f8677e7945f55d0fdce7e827a4859675af
Michael Hall <hall@michaelhall.se>
2017-05-23 22:37:15 +0200

    Add UrlField class

---
+++ src/UrlField.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * This file is a part of the bluemvc-forms package.
+ *
+ * Read more at https://bluemvc.com/
+ */
+
+namespace BlueMvc\Forms;
+
+use BlueMvc\Forms\Base\AbstractInputField;
+use DataTypes\Interfaces\UrlInterface;
+use DataTypes\Url;
+
+/**
+ * Class representing a url field.
+ *
+ * @since 1.0.0
+ */
+class UrlField extends AbstractInputField
+{
+    /**
+     * Constructs the url field.
+     *
+     * @since 1.0.0
+     *
+     * @param string            $name  The name.
+     * @param UrlInterface|null $value The value.
+     *
+     * @throws \InvalidArgumentException If any of the $name parameter is not a string.
+     */
+    public function __construct($name, UrlInterface $value = null)
+    {
+        parent::__construct($name, $value !== null ? $value->__toString() : '');
+
+        $this->myValue = $value;
+    }
+
+    /**
+     * Returns the value of the url field.
+     *
+     * @since 1.0.0
+     *
+     * @return UrlInterface|null The value of the url field.
+     */
+    public function getValue()
+    {
+        return $this->myValue;
+    }
+
+    /**
+     * Returns the input type.
+     *
+     * @since 1.0.0
+     *
+     * @return string The input type.
+     */
+    protected function getType()
+    {
+        return 'url';
+    }
+
+    /**
+     * Called when value is set from form.
+     *
+     * @since 1.0.0
+     *
+     * @param string $value The value from form.
+     */
+    protected function onSetFormValue($value)
+    {
+        $value = trim($value);
+        $this->myValue = Url::tryParse($value); // fixme: validation
+        $this->setDisplayValue($value);
+    }
+
+    /**
+     * @var UrlInterface|null My value.
+     */
+    private $myValue;
+}
---
+++ tests/UrlFieldTest.php
@@ -0,0 +1,226 @@
+<?php
+
+namespace BlueMvc\Forms\Tests;
+
+use BlueMvc\Forms\UrlField;
+use DataTypes\Url;
+
+/**
+ * Test UrlField class.
+ */
+class UrlFieldTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test basic constructor.
+     */
+    public function testBasicConstructor()
+    {
+        $urlField = new UrlField('foo');
+
+        self::assertSame('<input type="url" name="foo" required>', $urlField->getHtml());
+        self::assertSame('<input type="url" name="foo" required>', $urlField->__toString());
+    }
+
+    /**
+     * Test constructor with invalid name parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $name parameter is not a string.
+     */
+    public function testConstructorWithInvalidNameParameterType()
+    {
+        new UrlField(0);
+    }
+
+    /**
+     * Test getName method.
+     */
+    public function testGetName()
+    {
+        $urlField = new UrlField('foo');
+
+        self::assertSame('foo', $urlField->getName());
+    }
+
+    /**
+     * Test getValue method.
+     */
+    public function testGetValue()
+    {
+        $urlField = new UrlField('foo');
+
+        self::assertNull(null, $urlField->getValue());
+    }
+
+    /**
+     * Test setFormValue method.
+     */
+    public function testSetFormValue()
+    {
+        $urlField = new UrlField('foo');
+        $urlField->setFormValue('https://domain.com/');
+
+        self::assertSame('https://domain.com/', $urlField->getValue()->__toString());
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/" required>', $urlField->getHtml());
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/" required>', $urlField->__toString());
+    }
+
+    /**
+     * Test that setFormValue method trims the input.
+     */
+    public function testSetFormValueTrimsInput()
+    {
+        $urlField = new UrlField('foo');
+        $urlField->setFormValue(' https://domain.com/ ');
+
+        self::assertSame('https://domain.com/', $urlField->getValue()->__toString());
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/" required>', $urlField->getHtml());
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/" required>', $urlField->__toString());
+    }
+
+    /**
+     * Test setFormValue method with invalid value parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $value parameter is not a string.
+     */
+    public function testSetFormValueWithInvalidValueParameterType()
+    {
+        $urlField = new UrlField('foo');
+        $urlField->setFormValue(true);
+    }
+
+    /**
+     * Test that output is html-encoded.
+     */
+    public function testOutputIsHtmlEncoded()
+    {
+        $urlField = new UrlField('<p>');
+        $urlField->setFormValue('<em>');
+
+        self::assertSame('<input type="url" name="&lt;p&gt;" value="&lt;em&gt;" required>', $urlField->getHtml());
+        self::assertSame('<input type="url" name="&lt;p&gt;" value="&lt;em&gt;" required>', $urlField->__toString());
+    }
+
+    /**
+     * Test isEmpty method for empty value.
+     */
+    public function testIsEmptyForEmptyValue()
+    {
+        $urlField = new UrlField('foo');
+
+        self::assertTrue($urlField->isEmpty());
+    }
+
+    /**
+     * Test isEmpty method for non-empty value.
+     */
+    public function testIsEmptyForNonEmptyValue()
+    {
+        $urlField = new UrlField('foo');
+        $urlField->setFormValue('https://localhost:8080/');
+
+        self::assertFalse($urlField->isEmpty());
+    }
+
+    /**
+     * Test hasError method.
+     */
+    public function testHasError()
+    {
+        $urlField = new UrlField('foo');
+
+        self::assertFalse($urlField->hasError());
+    }
+
+    /**
+     * Test getError method.
+     */
+    public function testGetError()
+    {
+        $urlField = new UrlField('foo');
+
+        self::assertNull($urlField->getError());
+    }
+
+    /**
+     * Test setError method.
+     */
+    public function testSetError()
+    {
+        $urlField = new UrlField('foo');
+        $urlField->setError('My Error');
+
+        self::assertTrue($urlField->hasError());
+        self::assertSame('My Error', $urlField->getError());
+    }
+
+    /**
+     * Test setError method with invalid parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $error parameter is not a string.
+     */
+    public function testSetErrorWithInvalidParameterType()
+    {
+        $urlField = new UrlField('foo');
+        $urlField->setError(1.2);
+    }
+
+    /**
+     * Test constructor with default value.
+     */
+    public function testConstructorWithDefaultValue()
+    {
+        $urlField = new UrlField('foo', Url::parse('https://domain.com/foo/bar'));
+
+        self::assertSame('https://domain.com/foo/bar', $urlField->getValue()->__toString());
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/foo/bar" required>', $urlField->getHtml());
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/foo/bar" required>', $urlField->__toString());
+    }
+
+    /**
+     * Test getHtml method with attributes.
+     */
+    public function testGetHtmlWithAttributes()
+    {
+        $urlField = new UrlField('foo', Url::parse('https://domain.com/foo/bar'));
+
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/foo/bar" required id="baz" readonly>', $urlField->getHtml(['id' => 'baz', 'readonly' => true]));
+    }
+
+    /**
+     * Test isRequired method.
+     */
+    public function testIsRequired()
+    {
+        $urlField = new UrlField('foo');
+
+        self::assertTrue($urlField->isRequired());
+    }
+
+    /**
+     * Test setRequired method.
+     */
+    public function testSetRequired()
+    {
+        $urlField = new UrlField('foo', Url::parse('https://domain.com/foo/bar'));
+        $urlField->setRequired(false);
+
+        self::assertFalse($urlField->isRequired());
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/foo/bar">', $urlField->getHtml());
+        self::assertSame('<input type="url" name="foo" value="https://domain.com/foo/bar">', $urlField->__toString());
+    }
+
+    /**
+     * Test setRequired method with invalid parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $isRequired parameter is not a boolean.
+     */
+    public function testSetRequiredWithInvalidParameterType()
+    {
+        $urlField = new UrlField('foo');
+        $urlField->setRequired(0);
+    }
+}
