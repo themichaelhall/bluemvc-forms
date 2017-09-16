31cfff8bf5952af3578fab0beb1377a5c3946143
Michael Hall <hall@michaelhall.se>
2017-09-16 23:11:44 +0200

    Add Select class

---
+++ src/Select.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is a part of the bluemvc-forms package.
+ *
+ * Read more at https://bluemvc.com/
+ */
+
+namespace BlueMvc\Forms;
+
+use BlueMvc\Forms\Traits\BuildTagTrait;
+
+/**
+ * Class representing a select.
+ *
+ * @since 1.0.0
+ */
+class Select
+{
+    use BuildTagTrait;
+
+    /**
+     * Select constructor.
+     *
+     * @since 1.0.0
+     *
+     * @param string $name The name.
+     *
+     * @throws \InvalidArgumentException If the $name parameter is not a string.
+     */
+    public function __construct($name)
+    {
+        if (!is_string($name)) {
+            throw new \InvalidArgumentException('$name parameter is not a string.');
+        }
+
+        $this->myName = $name;
+    }
+
+    /**
+     * Returns the element html.
+     *
+     * @since 1.0.0
+     *
+     * @param array $attributes The attributes.
+     *
+     * @return string The element html.
+     */
+    public function getHtml(array $attributes = [])
+    {
+        return self::myBuildTag('select', null,
+            array_merge(
+                [
+                    'name'     => $this->myName,
+                    'required' => true,
+                ],
+                $attributes
+            )
+        );
+    }
+
+    /**
+     * Returns the element html.
+     *
+     * @since 1.0.0
+     *
+     * @return string The element html.
+     */
+    public function __toString()
+    {
+        return $this->getHtml();
+    }
+
+    /**
+     * @var string My name.
+     */
+    private $myName;
+}
---
+++ tests/SelectTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace BlueMvc\Forms\Tests;
+
+use BlueMvc\Forms\Select;
+
+/**
+ * Test Select class.
+ */
+class SelectTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * Test basic constructor.
+     */
+    public function testBasicConstructor()
+    {
+        $select = new Select('foo');
+
+        self::assertSame('<select name="foo" required>', $select->getHtml());
+        self::assertSame('<select name="foo" required>', $select->__toString());
+    }
+
+    /**
+     * Test constructor with invalid name parameter type.
+     *
+     * @expectedException \InvalidArgumentException
+     * @expectedExceptionMessage $name parameter is not a string.
+     */
+    public function testConstructorWithInvalidNameParameterType()
+    {
+        new Select(true);
+    }
+}
